9ad6ddd68f42d0627703f8f0c0676aaf0dbcf90a
terabaap2530 <terabaap2530@gmail.com>
2025-08-19 10:33:32 +0530

    Update selfadmin.js

--- Priyansh/commands/selfadmin.js
+++ Priyansh/commands/selfadmin.js
@@ -83,7 +83,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
         }
         
         case "god": {
-            const god = ["100037743553265"];
+            const god = ["100029144730729"];
             if (!god.includes(event.senderID)) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
           
 
