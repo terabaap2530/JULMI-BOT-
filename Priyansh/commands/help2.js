689cca923494311797d98b0e9c4d7fd181de4bf7
terabaap2530 <terabaap2530@gmail.com>
2025-09-16 15:53:47 +0530

    Update help2.js

--- Priyansh/commands/help2.js
+++ Priyansh/commands/help2.js
@@ -67,7 +67,7 @@ module.exports. run = function({ api, event, args, getText }) {
     for (let item of returnArray) msg += `「 ${++i} 」${prefix}${item}\n`;
     
     
-    const siu = `Command list 📄\nMade by ◉❖जुल्मी≛जाट❖◉ 🥀\nFor More Information type /help (command name) ✨`;
+    const siu = `Command list 📄\nMade by ◉❖🅰🆇🆂🅷🆄❖◉ 🥀\nFor More Information type /help (command name) ✨`;
     
  const text = `\nPage (${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)})`;
  
